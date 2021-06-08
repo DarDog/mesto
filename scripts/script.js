fff212d8da4252518333c7109639e60bb16a99bf
DarDog <samediacc666@gmail.com>
2021-06-09 02:26:44 +1100

    feat: Сделать скрипты сохранения данных из формы

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 let popUp = document.querySelector('.pop-up'),
-    closeButton = popUp.querySelector('.pop-up__exit-button'),
+    formElement = popUp.querySelector('.pop-up__container'),
+    closeButton = formElement.querySelector('.pop-up__exit-button'),
     editButton = document.querySelector('.profile__edit-button'),
-    nameInput = document.querySelector('#name-input'),
-    descriptionInput = document.querySelector('#description-input');
+    nameInput = formElement.querySelector('#name-input'),
+    descriptionInput = formElement.querySelector('#description-input'),
+    profileName = document.querySelector('.profile__title'),
+    profileDescription = document.querySelector('.profile__subtitle');
 
 /*Функция заполнение инпутов данными из профиля*/
 function fillInputs() {
-  nameInput.value = document.querySelector('.profile__title').textContent;
-  descriptionInput.value = document.querySelector('.profile__subtitle').textContent;
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
 }
 
 /*Функция открытия pop-up*/
@@ -21,8 +24,19 @@ function closePopUp() {
   popUp.classList.remove('pop-up_opened');
 }
 
+/*Функция сохранения данных из формы*/
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closePopUp();
+}
+
 /*Закрытие pop-up по клику*/
 editButton.addEventListener('click', openPopUp);
 
 /*Открытие pop-up по клику*/
 closeButton.addEventListener('click', closePopUp);
+
+/*Сохранение данных из формы*/
+formElement.addEventListener('submit', formSubmitHandler);

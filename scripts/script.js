85eb75609a194609ad401fce5451202c50b223f6
DarDog <samediacc666@gmail.com>
2021-06-09 02:09:03 +1100

    feat: Сделать скрипты заполнения инпутов pop-up

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
 let popUp = document.querySelector('.pop-up'),
     closeButton = popUp.querySelector('.pop-up__exit-button'),
-    editButton = document.querySelector('.profile__edit-button');
+    editButton = document.querySelector('.profile__edit-button'),
+    nameInput = document.querySelector('#name-input'),
+    descriptionInput = document.querySelector('#description-input');
 
-/*Функция закрытия pop-up*/
-function closePopUp() {
-  popUp.classList.remove('pop-up_opened')
+/*Функция заполнение инпутов данными из профиля*/
+function fillInputs() {
+  nameInput.value = document.querySelector('.profile__title').textContent;
+  descriptionInput.value = document.querySelector('.profile__subtitle').textContent;
 }
 
 /*Функция открытия pop-up*/
 function openPopUp() {
-  popUp.classList.add('pop-up_opened')
+  popUp.classList.add('pop-up_opened');
+  fillInputs();
+}
+
+/*Функция закрытия pop-up*/
+function closePopUp() {
+  popUp.classList.remove('pop-up_opened');
 }
 
 /*Закрытие pop-up по клику*/
